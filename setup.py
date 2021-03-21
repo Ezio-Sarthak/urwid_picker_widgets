c2179720a890a12553c56c6fcf28cf2a10746858
Ezio-Sarthak <garg.sarthak13@gmail.com>
2021-03-21 21:00:36 +0530

    setup: Bump release version.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name="urwid_picker_widgets",
-      version="0.1",
+      version="0.2",
       description="Specialized picker widgets for urwid "
                   "that extend its features.",
       long_description=long_description,
